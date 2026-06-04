4d3a6ae2888f39c02c87a0c607e312f2776b5d19
tkpremier <kkim31@gmail.com>
2026-06-04 12:39:39 -0400

    wrapped DriveDb with Suspense

--- src/app/drive-db/page.tsx
+++ src/app/drive-db/page.tsx
@@ -1,5 +1,8 @@
 import { getDrive } from '@/services/db/drive';
+import { Suspense } from 'react';
 import Grid from './Grid';
+
+const Loading = () => <div>Loading DriveDb</div>
 
 const Drive = async () => {
   const response = await getDrive();
@@ -9,7 +12,9 @@ const Drive = async () => {
     <>
       <h2>Welcome to the &#x1F608;</h2>
       <p>Here&apos;s what we&apos;ve been up to....</p>
-      <Grid driveData={data} />
+      <Suspense fallback={<Loading />}>
+        <Grid driveData={data} />
+      </Suspense>
     </>
   );
 };
